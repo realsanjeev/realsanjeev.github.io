f7f5e16818faaa45bd4dd98c7f91f618be97eb27
sanjeev.bhandari <sanjeev.bhandari@tai.com.np>
2025-06-25 09:36:21 +0545

    [portfolio]: Changed repo name to that of standard for portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-const repoName = "realsanjeev-portfolio"
+const repoName = "realsanjeev.github.io"
 
 // https://vitejs.dev/config/
 export default defineConfig
